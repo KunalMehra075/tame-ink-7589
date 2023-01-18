6473893c756285a0d44b99761a8297a8cf3a4019
KunalMehra075 <112753481+KunalMehra075@users.noreply.github.com>
2023-01-18 17:59:34 +0530

    One Product Page Done

--- Frontend/JS/Allproducts.js
+++ Frontend/JS/Allproducts.js
@@ -8,12 +8,12 @@ lth.addEventListener("click", () => {
   htl.classList.remove("htlid");
   lth.classList.add("htlid");
 });
-let hmm = document.getElementsByClassName("ChildBoxes");
-for (let i = 0; i < hmm.length; i++) {
-  hmm[i].addEventListener("mouseenter", () => {
-    hmm[i].children[1].children[12].style.display = "block";
-  });
-  hmm[i].addEventListener("mouseleave", () => {
-    hmm[i].children[1].children[12].style.display = "none";
-  });
-}
+// let hmm = document.getElementsByClassName("ChildBoxes");
+// for (let i = 0; i < hmm.length; i++) {
+//   hmm[i].addEventListener("mouseenter", () => {
+//     hmm[i].children[1].children[12].style.display = "block";
+//   });
+//   hmm[i].addEventListener("mouseleave", () => {
+//     hmm[i].children[1].children[12].style.display = "none";
+//   });
+// }
